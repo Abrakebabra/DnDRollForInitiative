522c7049ecfd1478a05f0d32399f801219487c5c
Keith Lee <lee.mingkeith@gmail.com>
2019-09-04 13:55:52 +0900

    Initial Commit

---
+++ DnDRollForInitiative/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DnDRollForInitiative
+//
+//  Created by Keith Lee on 2019/09/04.
+//  Copyright © 2019 Keith Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
